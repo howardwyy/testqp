b548f8d0e6c5a160452c28d5f2b76ae3eefdf784
howardwyy <wancjnzdm@126.com>
2014-09-11 21:25:16 +0800

    womodel

---
+++ BarcodeAPI/BarcodeModel/MODEL/Barcode/FG/WOModel.cs
@@ -0,0 +1,28 @@
+﻿using BarcodeModel.ADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeModel.MODEL.Barcode.FG
+{
+    [Tablename(TableName = "view_WO", PrimaryKey = "MP64001", ViewName = "view_WO", IsScalaDB = true)]
+    public class WOModel:BaseSearchModel
+    {
+        [Columname(Name="MP64001")]
+        public string WorkOrder { get; set; }
+
+        [Columname(Name = "MP64002")]
+        public string StockCode { get; set; }
+
+        [Columname(Name = "MP64004")]
+        public decimal PlanQTY { get; set; }
+
+        [Columname(Name = "MP64005")]
+        public decimal Acqty { get; set; }
+
+        [Columname(Name = "Company")]
+        public string Company { get; set; }
+    }
+}
