127a62cb2ca32a40e8868ae445f1314b37b36f35
howardwyy <wancjnzdm@126.com>
2014-09-12 12:41:42 +0800

    solinemodel

--- BarcodeAPI/BarcodeModel/MODEL/Barcode/FG/SOLineModel.cs
+++ BarcodeAPI/BarcodeModel/MODEL/Barcode/FG/SOLineModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BarcodeModel.ADO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace BarcodeModel.MODEL.Barcode.FG
 {
-    class SOLineModel
+    [Tablename(TableName = "view_SOLine", PrimaryKey = "OR01001", ViewName = "view_SOLine", IsScalaDB = true)]
+    public class SOLineModel
     {
+        [Columname(Name="OR01001")]
+        public string OrderNumber { get; set; }
+        
+        [Columname(Name="OR03002")]
+        public string LineNumber { get; set; }
+        
+        [Columname(Name="OR03005")]
+        public string StockCode { get; set; }
+        
+        [Columname(Name="OR03006")]
+        public string StockName { get; set; }
+        
+        [Columname(Name="OR03007")]
+        public string StockSpec { get; set; }
+        
+        [Columname(Name="UNIT")]
+        public string Unit { get; set; }
+        
+        [Columname(Name="OR03011")]
+        public string OrderQty { get; set; }
+        
+        [Columname(Name="OR01003")]
+        public string CustomerCode { get; set; }
+        
+        [Columname(Name="SL01002")]
+        public string CustomerName { get; set; }
+        
+        [Columname(Name="Company")]
+        public string Company { get; set; }
+        
+        [Columname(Name="OR01015")]
+        public DateTime OrderDate { get; set; }
+
+        [Columname(Name = "OR01008")]
+        public decimal Price { get; set; }
     }
 }
